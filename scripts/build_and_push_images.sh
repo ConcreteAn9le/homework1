bee58410bcf6682e6259eede905cb59f8fb30bdd
lei_dang <lei.dang@thoughtworks.com>
2021-12-05 20:09:23 +0800

    modify .sh file with credentials

--- scripts/build_and_push_images.sh
+++ scripts/build_and_push_images.sh
@@ -2,7 +2,7 @@
 
 docker build -t hello-world .
 
-aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com
+aws ecr get-login-password --region ap-southeast-1 --profile toc | docker login --username AWS --password-stdin 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com
 
 docker tag hello-world:1.0 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/bar
 
