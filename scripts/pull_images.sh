07267ee43c9d8fbd550af4493618164949e33be6
lei_dang <lei.dang@thoughtworks.com>
2021-12-05 21:18:45 +0800

    modify pull_images_and_run.sh

--- scripts/pull_images.sh
+++
@@ -1,1 +0,0 @@
-docker pull 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/bar
---
+++ scripts/pull_images_and_run.sh
@@ -0,0 +1,3 @@
+docker pull 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/bar
+
+java -jar ./build/libs/homework1-1.0-SNAPSHOT.jar
