ecde406b25a832a5111febadab065142dcebbb59
lei_dang <lei.dang@thoughtworks.com>
2021-12-05 22:47:09 +0800

    build new images named test:1.0

--- scripts/build_and_push_images.sh
+++ scripts/build_and_push_images.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker build -t hello-world .
+docker build -t test:1.0 .
 
 aws ecr get-login-password --region ap-southeast-1 --profile toc | docker login --username AWS --password-stdin 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com
 
-docker tag hello-world:1.0 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/bar
+docker tag test:1.0 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/bar
 
 docker push 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/bar
