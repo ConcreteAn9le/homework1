c99768faa73dcb4a76ff3b0fcf8d5f31c44ed555
lei_dang <lei.dang@thoughtworks.com>
2021-12-06 10:12:54 +0800

    trigger pipline

--- scripts/build_and_push_images.sh
+++ scripts/build_and_push_images.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker build -t test:1.0 .
+docker build -t demo:1.0 .
 
 aws ecr get-login-password --region ap-southeast-1 --profile toc | docker login --username AWS --password-stdin 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com
 
-docker tag test:1.0 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/bar
+docker tag demo:1.0 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/bar
 
 docker push 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/bar
