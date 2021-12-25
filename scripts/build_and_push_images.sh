b0020aa988e8cbd6a1020c11f61dbd75e4b4af12
lei_dang <lei.dang@thoughtworks.com>
2021-12-25 18:40:58 +0800

    change image name in scripts

--- scripts/build_and_push_images.sh
+++ scripts/build_and_push_images.sh
@@ -4,6 +4,6 @@ docker build -t homework1:1.0 .
 
 aws ecr get-login-password --region ap-southeast-1 --profile toc | docker login --username AWS --password-stdin 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com
 
-docker tag demo:1.0 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/dl-repo
+docker tag homework1:1.0 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/dl-repo
 
 docker push 494526681395.dkr.ecr.ap-southeast-1.amazonaws.com/dl-repo
